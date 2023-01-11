41fff2910e232b0286d010e841af748976af221b
Atul Tiwari <tiwariatul9526@gmail.com>
2023-01-11 19:25:11 +0530

    creating product model

--- backend/controllers/productController.controllers.js
+++ backend/controllers/productController.controllers.js
@@ -1,3 +1,9 @@
+const Product = require('../models/productModel.models')
+
+//create product
+
+exports.createProduct = async (req, res, next) => {}
+
 exports.getAllProducts = (req, res) => {
   res.status(200).json({ message: 'Route is working fine' })
 }
