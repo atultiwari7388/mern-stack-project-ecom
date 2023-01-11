f4d1ffe20e4f8ae9f17950bce8773b5979db7f07
Atul Tiwari <tiwariatul9526@gmail.com>
2023-01-12 01:26:14 +0530

    creating product(get,post,update,delete)requests

--- backend/routes/productRoute.routes.js
+++ backend/routes/productRoute.routes.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const {
   getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteproduct,
+  getProductDetails,
 } = require('../controllers/productController.controllers')
 
 const router = express.Router()
 
 //creating routes
 
+//get request
 router.route('/products').get(getAllProducts)
+//post request
+router.route('/products/new').post(createProduct)
+//update
+router.route('/product/:id').put(updateProduct)
+//delete
+router.route('/product/:id').delete(deleteproduct)
+//getproductdetails
+router.route('/product/:id').get(getProductDetails)
 
 module.exports = router
